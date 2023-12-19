1870594ee6abac25ad28c0b0c437dbab74ca8e25
taoaustin <taoaustin@yahoo.ca>
2023-12-19 18:08:47 -0500

    day 17: bane of my existence

--- 17/main.py
+++ 17/main.py
@@ -1,40 +1,31 @@
 import math
 import heapq
 
-LEFT = (0, -1)
-RIGHT = (0, 1)
-UP = (-1, 0)
-DOWN = (1, 0)
-
-def left_of(vec_row, vec_col): return (-vec_col, vec_row)
-def right_of(vec_row, vec_col): return (vec_col, -vec_row)
-def get_i(vec_row, vec_col, width): return vec_row * width + vec_col
-def get_coord(i, width): return (i // width, i % width)
-def is_valid_coord(vec_row, vec_col, width, height):
-    return vec_row >= 0 and vec_row < height and vec_col >= 0 and vec_col < width
-def add_coord(x, y): return tuple(map(lambda i, j: i + j, x, y))
+def get_i(vec, width): return vec[0] * width + vec[1]
+def is_valid(vec, width, height):
+    return vec[0] >= 0 and vec[0] < height and vec[1] >= 0 and vec[1] < width
 
 def get_neighbours(v, dir, width, height, dir_times, min, max): 
     if v == 0:
-        return ((1, RIGHT), (width, DOWN))
-    v_coord = get_coord(v, width)
-    forward_v = add_coord(v_coord, dir)
-    left_dir = left_of(*dir)
-    left_v = add_coord(v_coord, left_dir)
-    right_dir = right_of(*dir)
-    right_v = add_coord(v_coord, right_dir)
+        return ((1, (0, 1)), (width, (1, 0)))
+    v_coord = (v // width, v % width)
+    forward_v = (v_coord[0] + dir[0], v_coord[1] + dir[1])
+    left_dir = (-dir[1], dir[0])
+    left_v = (v_coord[0] + left_dir[0], v_coord[1] + left_dir[1])    
+    right_dir = (dir[1], - dir[0])
+    right_v = (v_coord[0] + right_dir[0], v_coord[1] + right_dir[1])    
     res = []
     if (dir_times < min):
-        if (is_valid_coord(forward_v[0], forward_v[1], width, height)):
-            res.append((get_i(forward_v[0], forward_v[1], width), dir))
+        if (is_valid(forward_v, width, height)):
+            res.append((get_i(forward_v, width), dir))
         return tuple(res)
 
-    if (is_valid_coord(forward_v[0], forward_v[1], width, height) and dir_times < max):
-        res.append((get_i(forward_v[0], forward_v[1], width), dir))
-    if (is_valid_coord(left_v[0], left_v[1], width, height)):
-        res.append((get_i(left_v[0], left_v[1], width), left_dir))
-    if (is_valid_coord(right_v[0], right_v[1], width, height)):
-        res.append((get_i(right_v[0], right_v[1], width), right_dir))
+    if (is_valid(forward_v, width, height) and dir_times < max):
+        res.append((get_i(forward_v, width), dir))
+    if (is_valid(left_v, width, height)):
+        res.append((get_i(left_v, width), left_dir))
+    if (is_valid(right_v, width, height)):
+        res.append((get_i(right_v, width), right_dir))
     return tuple(res)
 
 def dijkstra(graph, source, width, height, min, max):
@@ -42,15 +33,13 @@ def dijkstra(graph, source, width, height, min, max):
     dists = [math.inf for _ in range(len(graph))]
     pq = [(0, source, (0, 0), 0)]
     while pq:
-        v_tup = heapq.heappop(pq)
-        (dist, v, dir, dir_times) = v_tup
-        if (dir_times, v, dir) in seen:
+        (dist, v, dir, dir_times) = heapq.heappop(pq)
+        if (v, dir, dir_times) in seen:
             continue
-        seen.add((dir_times, v, dir))
+        seen.add((v, dir, dir_times))
         for u, u_dir in get_neighbours(v, dir, width, height, dir_times, min, max):
             l_uv = graph[u]
-            if (u_dir == dir): new_times = dir_times + 1
-            else: new_times = 1
+            new_times = dir_times + 1 if u_dir == dir else 1
             if (l_uv + dist < dists[u] and new_times >= min):
                 dists[u] = l_uv + dist
             heapq.heappush(pq, tuple((l_uv + dist, u, u_dir, new_times)))
@@ -67,3 +56,6 @@ def main():
      
 if __name__ == "__main__":
     main()
+    # Ended up being more like dp than greedy dijkstra :/
+    # Can't mark a vertex as visited (seen). 
+    # But you can mark a state: (vertex, prev-direction, #-of-times-of-prev-direction) visited.
