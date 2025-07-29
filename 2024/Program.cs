6eef1cb02a928d873c78cf89d3ccf6f400c2f187
taoaustin <taoaustin@yahoo.ca>
2025-07-29 00:58:29 -0400

    05

--- 2024/Program.cs
+++ 2024/Program.cs
@@ -7,7 +7,8 @@
       // var result = Day1.Solve();
       // var result = Day2.Solve();
       // var result = Day3.Solve();
-      var result = Day4.Solve();
+      // var result = Day4.Solve();
+      var result = Day5.Solve();
       Console.WriteLine(result);
     }
   }
