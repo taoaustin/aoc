59d879fc7aec53b9c7c366eeda640835579a27ee
taoaustin <taoaustin@yahoo.ca>
2025-07-29 01:05:13 -0400

    5 golfy

--- 2024/05/Day5.cs
+++ 2024/05/Day5.cs
@@ -6,21 +6,12 @@ namespace AdventOfCode2024
     {
       string[] file = Utils.ReadFileLines(5, false);
       var (rules, prints) = CreateRules(file);
-      int validMids = 0;
-      int fixedMids = 0;
+      int validMids = 0, fixedMids = 0;
       foreach (string printing in prints)
       {
-
         List<string> printOrder = [.. printing.Split(",")];
-
-        if (ApplyRules(printOrder, rules))
-        {
-          validMids += int.Parse(printOrder[printOrder.Count / 2]);
-        }
-        else
-        {
-          fixedMids += Fix(printOrder, rules);
-        }
+        if (ApplyRules(printOrder, rules)) validMids += int.Parse(printOrder[printOrder.Count / 2]);
+        else fixedMids += Fix(printOrder, rules);
       }
       return (validMids, fixedMids);
     }
@@ -30,26 +21,21 @@ namespace AdventOfCode2024
       List<string> newOrder = [];
       for (int i = 0; i < printOrder.Count; i++)
       {
-        var curNum = printOrder[i];
-        var curRules = rules.GetValueOrDefault(key: curNum, []);
+        var curRules = rules.GetValueOrDefault(key: printOrder[i], []);
         bool noInsert = true;
         for (int y = 0; y < newOrder.Count; y++)
         {
           if (curRules.Contains(newOrder[y]))
           {
-            newOrder.Insert(y, curNum);
+            newOrder.Insert(y, printOrder[i]);
             noInsert = false;
             break;
           }
         }
-        if (noInsert)
-        {
-          newOrder.Add(curNum);
-        }
+        if (noInsert) newOrder.Add(printOrder[i]);
       }
       return int.Parse(newOrder[newOrder.Count / 2]);
     }
-
 
     static bool ApplyRules(List<string> printOrder, Dictionary<string, List<string>> rules)
     {
@@ -57,10 +43,7 @@ namespace AdventOfCode2024
       {
         var seenSoFar = printOrder[..i];
         var intersect = seenSoFar.Intersect(rules.GetValueOrDefault(key: printOrder[i], []));
-        if (intersect.Any())
-        {
-          return false;
-        }
+        if (intersect.Any()) return false;
       }
       return true;
     }
@@ -72,20 +55,10 @@ namespace AdventOfCode2024
       for (i = 0; i < rules.Length; i++)
       {
         var rule = rules[i];
-        if (rule.Equals(""))
-        {
-          break;
-        }
-
+        if (rule.Equals("")) break;
         string[] parts = rule.Split(['|']);
-        if (ruleMap.TryGetValue(parts[0], out List<string>? value))
-        {
-          value.Add(parts[1]);
-        }
-        else
-        {
-          ruleMap.Add(parts[0], [parts[1]]);
-        }
+        if (ruleMap.TryGetValue(parts[0], out List<string>? value)) value.Add(parts[1]);
+        else ruleMap.Add(parts[0], [parts[1]]);
       }
       return (ruleMap, rules[(i + 1)..]);
     }
